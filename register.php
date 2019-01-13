1e421b199c6bd387e6f06d318f5bdbb56809fa96
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2019-01-13 13:58:05 +0100

    first one

--- register.php
+++ register.php
@@ -1,43 +1,10 @@
 <?php
-
-function pass($text1){
-    $text1=strip_tags($text1);
-    $text1=str_replace(" ","",$text1);
-    return $text1;
-}
-
-function _3adya($text1){
-    $text1=strip_tags($text1);
-    $text1= str_replace(" ","",$text1);
-    return $text1;
-}
-function _machi3adiya($text1){
-    $text1=strip_tags($text1);
-    $text1= str_replace(" ","",$text1);
-    $text1=ucfirst(strtolower($text1));
-    return $text1;
-}
+include('includes/handlers/register_handler.php');
 
 
 
-if(isset($_POST['loginbutton'])){
-      ///login form
-}
 
-if(isset($_POST['registerbutton'])){
- // register form
- 
 
-$username=_3adya($_POST['username']); 
-$firstname=_machi3adiya($_POST['firsname']); 
- $lastname=_machi3adiya($_POST['lastname']); 
- $email=_machi3adiya($_POST['email']); 
- $email2=_machi3adiya($_POST['email2']); 
- $password=pass($_POST['password']); 
- $password2=pass($_POST['password2']); 
- 
-
-}
 
 ?>
 
